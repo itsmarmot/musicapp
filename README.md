19920242484658cd6866fb83cd6f80ce7840b8de
muammar yasir <132113432+itsmarmot@users.noreply.github.com>
2023-10-23 10:54:41 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# MusicApp
+
+>  A Music App developed using JavaScript, CSS, and HTML. Users can click the Forward play, Backward play, Playlist loop and Shuffle music buttons to change the Music.
+ 
+## Live Demo
+
+[Live Demo Link](https://itsmarmot.github.io/musicapp) :point_left:
+
+
+---
+
+# made with ❤️ by Ammar
+> These are some of my favorite 25 songs with various genres and kinds of famous or hidden song. Happy listening and hopefully you like it too ♫₊˚.🎧
---
+++ js/music-list.js
@@ -0,0 +1,142 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the https://github.com/itsmarmot"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Payphone",
+    artist: "Maroon 5 ft. Wiz Khalifa",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "I Like It Like That ft. New Boyz",
+    artist: "Hot Chelle Rae",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Stay",
+    artist: "Zedd & Alessia Cara",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "FM Static - Tonight",
+    artist: "Alternative Music",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Titanium feat. Sia",
+    artist: "David Guetta",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Wasteland",
+    artist: "Against The Current",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Jackie Chan ft. Post Malone",
+    artist: "Tiësto & Dzeko",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Like A G6",
+    artist: "Far East Movement",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Leave Before You Love Me",
+    artist: "Marshmello",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Still Into You",
+    artist: "Paramore",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Into Your Arms - The Maine",
+    artist: "Can't Stop Won't Stop",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "You Are My Everything",
+    artist: "B-Rock 비록 & K-Brown of Timber",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Jewelry (쥬얼리) - Love Story",
+    artist: "Korean Music",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "たぶん (Probably/Tabun)",
+    artist: "YOASOBI",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "MY FIRST STORY - Missing You",
+    artist: "※ ANTITHESE ※",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "You Belong With Me",
+    artist: "Taylor Swift",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Hall Of Fame",
+    artist: "The Script",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Ed Sheeran ÷ Shape of You",
+    artist: "Ed Sheeran",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Love Like Woe",
+    artist: "The Ready Set",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Your Guardian Angel",
+    artist: "The Red Jumpsuit Apparatus",
+    img: "music-20",
+    src: "music-20"
+  },
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }    
+];
